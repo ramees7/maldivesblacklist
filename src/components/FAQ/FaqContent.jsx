813ef6161c0e96f47f3be1918e47b1dec3610352
ramees7 <144250876+ramees7@users.noreply.github.com>
2024-09-05 23:30:06 +0530

    some content removed

--- src/components/FAQ/FaqContent.jsx
+++ src/components/FAQ/FaqContent.jsx
@@ -46,30 +46,7 @@ export default function FaqContent() {
         </p>
       </div>
 
-      {/* Another FAQ Item */}
-      <div className="bg-white shadow-lg rounded-lg p-6">
-        <h2 className="text-lg font-semibold">I'm not sure what category to choose – what should I do?</h2>
-        <p className="mt-2 text-gray-600">
-          Choose the category that seems closest to your issue, and we'll review 
-          and classify it correctly.
-        </p>
-      </div>
-      <div className="bg-white shadow-lg rounded-lg p-6">
-        <h2 className="text-lg font-semibold">What can I report?</h2>
-        <p className="mt-2 text-gray-600">
-          Report anything you think may be a fraud, scam, or bad business practice. 
-          For ideas of what you might report, check out Blacklist Guide for more 
-          information and advice.
-        </p>
-      </div>
-
-      {/* Another FAQ Item */}
-      <div className="bg-white shadow-lg rounded-lg p-6">
-        <h2 className="text-lg font-semibold">I'm not sure if it's a scam or fraud – should I still report it?</h2>
-        <p className="mt-2 text-gray-600">
-          Yes, if you're unsure, it's better to report and let us investigate.
-        </p>
-      </div>
+     
 
       {/* Another FAQ Item */}
       <div className="bg-white shadow-lg rounded-lg p-6">
